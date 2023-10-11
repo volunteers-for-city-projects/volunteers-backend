f123db032cbcb9c9f4dbe9cb9e952d18790e4e08
Yuri Pomazkin <1yunker@gmail.com>
2023-10-12 01:19:12 +0500

    Add custom user model; update urls, settings, .gitignore and requirements

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
 
     'api.apps.ApiConfig',
     'content.apps.ContentConfig',
@@ -117,8 +120,49 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR, 'collected_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    # 'DEFAULT_FILTER_BACKENDS': [
+    #     'django_filters.rest_framework.DjangoFilterBackend',
+    # ],
+
+    # 'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
+    # 'PAGE_SIZE': 6,
+    # 'SEARCH_PARAM': 'name',
+
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'HIDE_USERS': False,
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    # 'SERIALIZERS': {'user': 'api.serializers.UserSerializer',
+    #                 'current_user': 'api.serializers.UserSerializer',
+    #                 'user_create': 'api.serializers.UserCreateSerializer', },
+    # 'PERMISSIONS': {'user': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    #                 'user_list': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    #                 'user_delete': ['rest_framework.permissions.IsAdminUser'], },
+}
+
+AUTH_USER_MODEL = 'users.User'
+
+# Constants
+MAX_LENGTH_NAME = 50
+MAX_LENGTH_SLUG = 50
+MAX_LENGTH_USERNAME = 50
+MAX_LENGTH_PASSWORD = 20
+MAX_LENGTH_EMAIL = 70
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,58 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+# from users.validators import username_validator
+
+USER_ROLES = [
+    ('admin', 'Администратор'),
+    ('organizer', 'Организатор'),
+    ('volunteer', 'Волонтер'),
+]
+
+
+class User(AbstractUser):
+    # username = models.CharField(
+    #     verbose_name='Пользователь',
+    #     max_length=settings.MAX_LENGTH_USERNAME,
+    #     unique=True,
+    #     validators=(UnicodeUsernameValidator(), username_validator,)
+    # )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=settings.MAX_LENGTH_NAME,
+        blank=False
+    )
+    second_name = models.CharField(
+        verbose_name='Отчество',
+        max_length=settings.MAX_LENGTH_NAME,
+        blank=False
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=settings.MAX_LENGTH_NAME,
+        blank=False
+    )
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        unique=True,
+        blank=False
+    )
+    role = models.CharField(
+        'Роль',
+        choices=USER_ROLES,
+        max_length=50,
+        default='volunteer'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('first_name', 'second_name', 'last_name',)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('last_name',)
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.second_name}'
