f0ce5699a135c25a89de65cad6230ac24383bf28
Ruzal <email.com>
2023-10-12 15:32:04 +0300

    updated project models

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+    MaxLengthValidator,
+    MinLengthValidator,
+)
+
 from django.core.validators import RegexValidator
+from datetime import date
 
 MAX_LEN_NAME = 200
 LEN_OGRN = 13
@@ -11,7 +18,11 @@ MESSAGE_PHONE_REGEX = (
 
 
 class Organization(models.Model):
-    # contact_person =
+    contact_person = models.OneToOneField(
+        # User,
+        related_name='organization',
+        verbose_name='Пользователь',
+    )
     title = models.CharField(
         max_length=MAX_LEN_NAME,
         blank=False,
@@ -19,8 +30,8 @@ class Organization(models.Model):
     )
     ogrn = models.PositiveIntegerField(
         validators=[
-            MaxValueValidator(LEN_OGRN),
-            MinValueValidator(LEN_OGRN),
+            MaxLengthValidator(LEN_OGRN),
+            MinLengthValidator(LEN_OGRN),
         ],
         unique=True,
         blank=False,
@@ -32,7 +43,7 @@ class Organization(models.Model):
                 regex=r'^(?:\+7|8)[0-9]{10}$',
                 message=MESSAGE_PHONE_REGEX.format(MAX_LEN_PHONE),
             ),
-            MaxValueValidator(MAX_LEN_PHONE),
+            MaxLengthValidator(MAX_LEN_PHONE),
         ],
         max_length=11,
         blank=True,
@@ -42,12 +53,12 @@ class Organization(models.Model):
         blank=False,
         verbose_name='Об организации',
     )
-    # city = models.OneToOneField(
-    #     City,
-    #     on_delete=models.CASCADE,
-    #     related_name='project',
-    #     verbose_name='Город',
-    # )
+    city = models.OneToOneField(
+        # City,
+        # on_delete=models.CASCADE,
+        related_name='organization',
+        verbose_name='Город',
+    )
 
     class Meta:
         verbose_name = 'Организация'
@@ -58,12 +69,100 @@ class Organization(models.Model):
 
 
 class Volunteer(models.Model):
+    user = models.OneToOneField(
+        # User,
+        related_name='volunteer',
+        verbose_name='Пользователь',
+    )
+    city = models.OneToOneField(
+        # City,
+        # on_delete=models.CASCADE,
+        related_name='volunteer',
+        verbose_name='Город',
+    )
+    telegram = models.CharField(
+        max_length=32,
+        validators=[
+            RegexValidator(
+                regex=r'^@[\w]+$',
+                message=(
+                    'Ник в Telegram должен начинаться с @ и содержать '
+                    'только буквы, цифры и знаки подчеркивания.',
+                ),
+            ),
+            MinLengthValidator(5),
+            MaxLengthValidator(32),
+        ],
+    )
+    skills = models.ForeignKey(
+        # Skills,
+        # on_delete=models.CASCADE,
+        verbose_name='Навыки',
+    )
+    photo = models.ImageField(
+        blank=False,
+        verbose_name='Фото',
+    )
+    activities = models.ForeignKey(
+        "app.Model",
+        verbose_name='Активности',
+        # on_delete=models.CASCADE,
+    )
+    date_of_birth = models.DateField(
+        blank=False,
+        null=False,
+        validators=[
+            MinValueValidator(limit_value=date(1900, 1, 1)),
+            MaxValueValidator(limit_value=date.today()),
+        ],
+        verbose_name='Дата рождения',
+        help_text='Введите дату в формате "ДД.ММ.ГГГГ", пример: "01 01 2000".',
+    )
+    phone = models.CharField(
+        validators=[
+            RegexValidator(
+                regex=r'^(?:\+7|8)[0-9]{10}$',
+                message=MESSAGE_PHONE_REGEX.format(MAX_LEN_PHONE),
+            ),
+            MaxLengthValidator(MAX_LEN_PHONE),
+        ],
+        max_length=11,
+        blank=True,
+        verbose_name='Телефон',
+    )
+
     class Meta:
         verbose_name = 'Волонтер'
         verbose_name_plural = 'Волонтеры'
 
     def __str__(self):
-        return self.name
+        return self.user
+
+
+class StatusApprove(models.Model):
+    APPROVED = 'approved'
+    PENDING = 'pending'
+    REJECTED = 'rejected'
+
+    STATUS_CHOICES = [
+        (APPROVED, 'Одобрено'),
+        (PENDING, 'На рассмотрении'),
+        (REJECTED, 'Отклонено'),
+    ]
+
+    title = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default=PENDING,
+        verbose_name='Статус проверки',
+    )
+
+    class Meta:
+        verbose_name = 'Статус проверки'
+        verbose_name_plural = 'Статусы проверки'
+
+    def __str__(self):
+        return self.title
 
 
 class Project(models.Model):
@@ -104,16 +203,50 @@ class Project(models.Model):
     #     verbose_name='Активности',
     # )
     organization = models
-    # city = models.OneToOneField(
-    #     City,
-    #     on_delete=models.CASCADE,
-    #     related_name='project',
-    #     verbose_name='Город',
-    # )
+    city = models.OneToOneField(
+        # City,
+        blank=False,
+        # on_delete=models.CASCADE,
+        related_name='project',
+        verbose_name='Город',
+    )
+    category = models.ForeignKey(
+        # Category,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='projects',
+        verbose_name='Категория',
+    )
+    photo_previous_event = models.ImageField(
+        blank=False,
+        verbose_name='Фото с мероприятия',
+    )
+    # tags =
+    participants = models.ForeignKey(
+        'ProjectParticipants',
+        verbose_name='',
+    )
+    status_approve = models.ForeignKey(
+        StatusApprove,
+        on_delete=models.CASCADE,
+        verbose_name='Статус проверки',
+    )
 
     class Meta:
         verbose_name = 'Проект'
         verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectParticipants(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Участник проекта'
+        verbose_name_plural = 'Участники проекта'
 
     def __str__(self):
         return self.name
