92ee3c47eee690581b9f263d669adeb814089cc9
Olga Komleva <olyakomleva@mail.ru>
2023-10-18 22:26:17 +0300

    Try

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('POSTGRES_DB', 'volunteers'),
-#         'USER': os.getenv('POSTGRES_USER', 'volunteers_user'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', 5432),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'volunteers'),
+        'USER': os.getenv('POSTGRES_USER', 'volunteers_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', 5432),
+    }
+}
 
 
 # Password validation
@@ -219,9 +219,9 @@ MIN_LEN_NAME_USER = 2
 MAX_LEN_NAME_USER = 40
 MESSAGE_NAME_USER_VALID = f'Длина поля от {MIN_LEN_NAME_USER} до {MAX_LEN_NAME_USER} символов'
 MESSAGE_NAME_USER_CYRILLIC = 'Введите имя кириллицей'
-MAX_LENGTH_ROLE = 50
-OGRN_ERROR_MESSAGE = 'ОГРН должен состоять из 13 цифр.'
 
+OGRN_ERROR_MESSAGE = 'ОГРН должен состоять из 13 цифр.'
+MAX_LENGTH_ROLE = 50
 MIN_LEN_TELEGRAM = 5
 MAX_LEN_TELEGRAM = 32
 TELEGRAM_ERROR_MESSAGE = 'Ник в Telegram должен начинаться с @ и содержать только буквы, цифры и знаки подчеркивания. От {} до {} символов.'
