5811a415440a2928aeda94c960fd1b6024c821c3
Olga Komleva <olyakomleva@mail.ru>
2023-11-02 21:09:10 +0300

    load csv files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,6 +175,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DATETIME_FORMAT': "%d.%m.%Y %H:%M",
     # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
