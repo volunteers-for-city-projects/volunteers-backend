e9b5949fcae4e023f653609d8bad66d0996e3642
Olga Komleva <olyakomleva@mail.ru>
2023-11-09 19:31:47 +0300

    create for projects

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import filters, generics, mixins, status, viewsets
 from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import SAFE_METHODS, AllowAny
 from rest_framework.response import Response
 from taggit.models import Tag
