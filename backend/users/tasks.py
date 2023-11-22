9419f03f28e5377738bdcede3f72734b25be6345
Kozin <grot_artem@inbox.ru>
2023-11-22 21:35:42 +0300

    Changed the filtering of users of the task and the schedule

--- backend/users/tasks.py
+++ backend/users/tasks.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
+from django.utils import timezone
 
 from backend import celery_app
 
@@ -8,5 +9,9 @@ User = get_user_model()
 
 @celery_app.task
 def delete_not_active_users():
+    tomorrow = timezone.now() - timezone.timedelta(days=1)
     with transaction.atomic():
-        User.objects.filter(is_active=False).delete()
+        User.objects.filter(
+            is_active=False,
+            date_joined__lt=tomorrow,
+        ).exclude(role=User.DELETED).delete()
